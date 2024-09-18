44dd3fac1247e1b073a8f947acaa4e84d0dfe611
ChandanaB5420 <chandanac653@gmail.com>
2024-09-18 19:43:00 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -3,3 +3,10 @@ def add(input_string):
         return 0
 
     return None  
+
+def add(input_string):
+    if input_string == "":
+        return 0
+    elif input_string == "0":
+        return 0
+    return None 
