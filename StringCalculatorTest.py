bc037c4d309893a625f8683c8118e9a4a16760ad
ChandanaB5420 <chandanac653@gmail.com>
2024-09-19 12:30:39 +0530

    Update StringCalculatorTest.py

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -20,7 +20,7 @@ class TestStringCalculator(unittest.TestCase):
         def test_expectSumWithNewlineDelimiter(self):
                 self.assertEqual(add("1\n2,3"),6);
 
-        # New test cases
+        # New test case
         def test_negativeNumbersRaiseException(self):
                 with self.assertRaises(ValueError) as context:
                     add("1,-2,-3") 
