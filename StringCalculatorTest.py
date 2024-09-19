11d64dc63859e999e66a1b066c8b312ebca2a045
ChandanaB5420 <chandanac653@gmail.com>
2024-09-19 12:27:02 +0530

    Update StringCalculatorTest.py

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -26,36 +26,29 @@ class TestStringCalculator(unittest.TestCase):
                 self.assertIn("Negative numbers are not allowed", str(context.exception))
 
     # New test cases
-        def test_ignoreInvalidDelimiter(self):
-        # Test case TC8
+        def test_ignoreInvalidDelimiter(self):       
                 with self.assertRaises(ValueError):
                     add("//\n1 2")  # Invalid delimiter (spaces)
 
         def test_ignoreNumbersGreaterThan1000WithMultipleInputs(self):
-        # Test case TC9
-            self.assertEqual(add("5,100,2000"), 105)
+                self.assertEqual(add("5,100,2000"), 105)
 
         def test_negativeNumbersRaiseDetailedException(self):
-        # Test case TC10
-                 with self.assertRaises(ValueError) as context:
+                with self.assertRaises(ValueError) as context:
                     add("1,-5,3,-10")
                 self.assertIn("Negative numbers are not allowed: [-5, -10]", str(context.exception))
 
         def test_expectSumWithCustomDelimiterAmpersand(self):
-        # Test case TC11
-                  self.assertEqual(add("//&\n1&2&1000"), 1003)
+                self.assertEqual(add("//&\n1&2&1000"), 1003)
 
         def test_newlineAsDelimiterWithTrailingComma(self):
-        # Test case TC12
-                 self.assertEqual(add("1,2,3,\n4"), 10)
+                self.assertEqual(add("1,2,3,\n4"), 10)
 
         def test_emptyTokensCountAsZero(self):
-        # Test case TC13
-                 self.assertEqual(add("1,,2"), 3)
+                self.assertEqual(add("1,,2"), 3)
 
         def test_ignoreGreaterThan1000InCustomDelimiter(self):
-        # Test case TC14
-                 self.assertEqual(add("//;\n1;2;3;1001"), 6)
+                self.assertEqual(add("//;\n1;2;3;1001"), 6)
 
 if __name__ == '__main__':
     unittest.main()
